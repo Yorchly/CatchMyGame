de1eab0192157c71442ed0a9fc1e5eecc61ed1ca
Jorge <jorge.gutierrezvila0@gmail.com>
2021-01-30 22:25:37 +0100

    Deleted search.py and changes in settings (CSE deleted)

--- app/settings.py
+++ app/settings.py
@@ -4,17 +4,6 @@ from app.models.games_cached import GamesCached
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-SEARCH_ENGINE_ID = os.environ.get("SEARCH_ENGINE_ID")
-
-# API KEY
-CUSTOM_SEARCH_API = os.environ.get("CUSTOM_SEARCH_API")
-
-SEARCH_URL = 'https://www.googleapis.com/customsearch/v1?cx={}&key={}&q='.format(
-    SEARCH_ENGINE_ID, CUSTOM_SEARCH_API
-)
-
-WEBS = ("eneba", "instantgaming", "steam store")
-
 GAMES_CACHED = GamesCached()
 
 ENEBA_FILENAME = "eneba.jl"
